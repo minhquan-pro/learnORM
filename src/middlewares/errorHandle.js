ef663ad671089af2a4c511888689d5d9247d8a81
Lê Minh Quân <minhquan0963580006@gmail.com>
2026-02-09 16:51:35 +0700

    feat: enhance error handling middleware with specific error responses

--- src/middlewares/errorHandle.js
+++ src/middlewares/errorHandle.js
@@ -1,5 +1,29 @@
-const errorHandle = (err, req, res, next) => {
-	res.error(String(err));
+const { PrismaClientValidationError } = require("@prisma/client/runtime/client");
+const isProduction = require("@/utils/isProduction");
+const { prismaCodes, httpCodes } = require("@/config/constants");
+
+const errorHandle = (err, req, res, _) => {
+	if (isProduction()) {
+		res.error("Server error.");
+	}
+
+	if (err instanceof PrismaClientValidationError) {
+		return res.error({
+			info: error,
+			message: String(error),
+		});
+	}
+
+	if (err?.code === prismaCodes.duplicate) {
+		return res.error(
+			{
+				message: "Duplicate entry",
+			},
+			httpCodes.conflict,
+		);
+	}
+
+	res.error(err ?? "Server Error");
 };
 
 module.exports = errorHandle;
